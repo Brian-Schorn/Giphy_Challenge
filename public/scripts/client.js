74ff882b48707be1f0af74127b8fcf21fde1c48f
Brian Schorn <schorn.brian@gmail.com>
2017-02-02 13:27:28 -0600

    Added Services

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,32 +1,50 @@
 var app = angular.module('giphyApp', []);
 
-app.controller('giphyController', function($http){
+app.controller('giphyController', function(giphyService){
   console.log("giphyController Loaded");
 
   var ctrl = this;
-  var API = "http://api.giphy.com/v1/";
-  var params = {params: {api_key: 'dc6zaTOxFJmzC'}};
-  ctrl.searchTerm = '';
 
-  //API KEYs , { params: { api_key: 'key goes here'}}
-  ctrl.randomGIF = function(){
-    $http.get(API + "gifs/random", params).then(function(response){
-      console.log(response);
-      ctrl.imageName = response.data.data.id;
-      ctrl.imageURL = response.data.data.image_url;
-      console.log('img URL: ', ctrl.imageURL);
+  // ctrl.searchTerm = '';
+
+
+  ctrl.randomGIF = function () {
+    console.log("Random GIF Request");
+    giphyService.randomGIF().then(function(gif){
+      ctrl.imageName = gif.data.id;
+      ctrl.imageURL = gif.data.image_url;
     });
   };
+  //API KEYs , { params: { api_key: 'key goes here'}}
+  // ctrl.randomGIF = function(){
+  //   $http.get(API + "gifs/random", params).then(function(response){
+  //     console.log(response);
+  //     ctrl.imageName = response.data.data.id;
+  //     ctrl.imageURL = response.data.data.image_url;
+  //     console.log('img URL: ', ctrl.imageURL);
+  //   });
+  // };
 
-  ctrl.searchGIF = function(){
-    params.params.q = ctrl.searchTerm.split(' ').join('+');
-    console.log(params.params.q);
-    $http.get(API + "gifs/search", params).then(function(response){
-      console.log(response);
-      ctrl.imageName = response.data.data[0].id;
-      ctrl.imageURL = response.data.data[0].images.original.url;
-      console.log('img URL: ', ctrl.imageURL);
-    });
-  }
+  // giphyService.searchGIF.then(function (gif){
+  //
+  // })
+
+ctrl.searchGIF = function(searchTerm) {
+  console.log("Search GIF Request", searchTerm);
+  giphyService.searchGIF(searchTerm).then(function(gif){
+      ctrl.imageName = gif.data[0].id;
+      ctrl.imageURL = gif.data[0].images.original.url;
+  })
+}
+  // ctrl.searchGIF = function(){
+  //   params.params.q = ctrl.searchTerm.split(' ').join('+');
+  //   console.log(params.params.q);
+  //   $http.get(API + "gifs/search", params).then(function(response){
+  //     console.log(response);
+  //     ctrl.imageName = response.data.data[0].id;
+  //     ctrl.imageURL = response.data.data[0].images.original.url;
+  //     console.log('img URL: ', ctrl.imageURL);
+  //   });
+  // }
 
 });
---
+++ public/scripts/giphyService.js
@@ -0,0 +1,50 @@
+app.service('giphyService', function ($http) {
+  var API = "http://api.giphy.com/v1/";
+  var params = {params: {api_key: 'dc6zaTOxFJmzC'}};
+
+
+  this.randomGIF = function () {
+    // return the promise to the caller
+    return $http.get(API + "gifs/random", params).then(function (response) {
+      console.log('Got a response from the API', response);
+      return response.data; // returned on resolution of promise
+    }).catch(function (err) {
+      console.log('Error getting info from API', err);
+    });
+  };
+
+
+  this.searchGIF = function(searchTerm){
+    params.params.q = searchTerm.split(' ').join('+');
+    console.log(params.params.q);
+
+    return $http.get(API + "gifs/search", params).then(function(response){
+      return response.data
+    }).catch(function (err) {
+      console.log('Error getting info from API after searching', err);
+    });
+  }
+  //API KEYs , { params: { api_key: 'key goes here'}}
+  // ctrl.randomGIF = function(){
+  //   $http.get(API + "gifs/random", params).then(function(response){
+  //     console.log(response);
+  //     ctrl.imageName = response.data.data.id;
+  //     ctrl.imageURL = response.data.data.image_url;
+  //     console.log('img URL: ', ctrl.imageURL);
+  //   });
+  // };
+
+
+
+
+  // this.getPokemon = function (pokemon) {
+  //   return $http.get(pokemon.url).then(function(response){
+  //     console.log('Pokemon info', response.data);
+  //     var foundPokemon =  response.data;
+  //     return foundPokemon.sprites.front_default; // image
+  //   }).catch(function(err){
+  //     console.log('Error getting info from API', err);
+  //   })
+  // };
+
+});
